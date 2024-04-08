288b5de0412b8930ed4ec88ddbe6bf4debd71491
AxelHallander <axel.hallander.7781@student.uu.se>
2024-04-08 17:44:16 +0200

    fixed all ruff format except one misstake

--- pqformula_test.py
+++ pqformula_test.py
@@ -1,6 +1,7 @@
 from pqformula import quad_equation
 
+
 def test_quad_equation():
-    assert quad_equation(2,1) == (-1,-1)
-    assert quad_equation(4,5) == ((-2+1j), (-2+1j))
-    assert quad_equation(-6,5) == (5,1)
+    assert quad_equation(2, 1) == (-1, -1)
+    assert quad_equation(4, 5) == ((-2 + 1j), (-2 + 1j))
+    assert quad_equation(-6, 5) == (5, 1)
