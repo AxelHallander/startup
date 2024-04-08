b592614ae8fdace3c10b17d6d3efbed9e4638de8
AxelHallander <axel.hallander.7781@student.uu.se>
2024-04-08 17:46:12 +0200

    ruff formula

--- pqformula.py
+++ pqformula.py
@@ -2,14 +2,14 @@ def quad_equation(p, q):
     """Function for second order pq-formula"""
     import math
 
-    disc = p*p/4 - q
+    disc = p * p / 4 - q
     if disc >= 0:
         d = math.sqrt(disc)
-        x1 = (-p/2 + d)
-        x2 = (-p/2 - d)
+        x1 = -p / 2 + d
+        x2 = -p / 2 - d
         return x1, x2
     else:
         d = math.sqrt(-disc)
-        x1 = complex(-p/2, d)
-        x2 = complex(-p/2, d)
+        x1 = complex(-p / 2, d)
+        x2 = complex(-p / 2, d)
         return x1, x2
