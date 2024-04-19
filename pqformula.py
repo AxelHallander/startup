8d98a1c63c68b09692e014c81ad008fc6cf7cdd1
Elisabeth2002S <e12001535@student.tuwien.ac.at>
2024-04-19 17:26:31 +0200

    try to fail 2

--- pqformula.py
+++ pqformula.py
@@ -6,7 +6,7 @@ def quad_equation(p, q):
     if disc >= 0:
         d = math.sqrt(disc)
         x1 = -p / 2 + d +4
-        x2 = -p / 2 - d
+        x2 = -p / 2 - d +21
         return x1, x2
     else:
         d = math.sqrt(-disc)
